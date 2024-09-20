46abe1c75df08b26dcf5388e0732343664c2848b
marekrams <marek.rams@uj.edu.pl>
2024-09-20 09:16:04 +0200

    update

--- operators.py
+++ operators.py
@@ -16,22 +16,22 @@ def HXXZ(N, a, m, ops=None):
     H = mps.generate_mpo(HI, terms)
     return H
 
-def Qp(n, t, x0, v, Q):
-    return Q * max(1 - abs(x0 + v * t - n), 0)
+def Qp(n, t, x0, a, v, Q):
+    return Q * max(1 - abs(x0 + v * t / a - n), 0)
 
-def Qm(n, t, x0, v, Q):
-    return Q * max(1 - abs(x0 - v * t - n), 0)
+def Qm(n, t, x0, a, v, Q):
+    return Q * max(1 - abs(x0 - v * t / a - n), 0)
 
-def dQn(n, t, x0, v, Q):
-    return sum(Qp(k, t, x0, v, Q) - Qm(k, t, x0, v, Q) for k in range(n + 1))
+def dQn(n, t, x0, a, v, Q):
+    return sum(Qp(k, t, x0, a, v, Q) - Qm(k, t, x0, a, v, Q) for k in range(n + 1))
 
-def hn(n, t, N, x0, L0, v, Q):
-    return N - 1 - n + 2 * ((-1) ** n) * sum(L0 + dQn(k, t, x0, v, Q) for k in range(n, N - 1))
+def hn(n, t, N, x0, L0, a, v, Q):
+    return N - 1 - n + 2 * ((-1) ** n) * sum(L0 + dQn(k, t, x0, a, v, Q) for k in range(n, N - 1))
 
 def set_x0(N):
     return (N - 1) / 2
 
-def sumLn2(N, t=0, L0=0, v=1, Q=1, ops=None):
+def sumLn2(N, t, L0, a, v, Q, ops=None):
     """ sum_{n=0}^{N-2} Ln^2 """
     x0 = set_x0(N)
     I, Sp, Sm = ops.I(), ops.sp(), ops.sm()
@@ -47,9 +47,9 @@ def sumLn2(N, t=0, L0=0, v=1, Q=1, ops=None):
     for n in range(N):
         An = ((-1) ** n) * d[n % 2]
         Bn = (((-1) ** n) * 2 * (N - 1 - n)) * d[n % 2]
-        Cn = hn(n, t, N, x0, L0, v, Q) * d[n % 2]
+        Cn = hn(n, t, N, x0, L0, a, v, Q) * d[n % 2]
         if n < N - 1:
-            Cn = Cn + ((L0 + dQn(n, t, x0, v, Q)) ** 2) * I
+            Cn = Cn + ((L0 + dQn(n, t, x0, a, v, Q)) ** 2) * I
         if n == 0:
             H[n] = yastn.block({(0, 0): I, (0, 1): An, (0, 2): Cn}, common_legs=(1, 3))
         elif n == N - 1:
@@ -62,11 +62,11 @@ def sumLn2(N, t=0, L0=0, v=1, Q=1, ops=None):
                                                         (2, 2): I}, common_legs=(1, 3))
     return H
 
-def Ln(n, N, t=0, L0=0, v=1, Q=1, ops=None):
+def Ln(n, N, t, L0, a, v, Q, ops=None):
     I, Sp, Sm = ops.I(), ops.sp(), ops.sm()
     d = {0: Sp @ Sm, 1: Sm @ Sp}
     terms = [mps.Hterm(((-1) ** k), [k], [d[k % 2]]) for k in range(n + 1)]
-    terms.append(mps.Hterm(L0 + dQn(n, t, set_x0(N), v, Q), [0], [I]))
+    terms.append(mps.Hterm(L0 + dQn(n, t, set_x0(N), a, v, Q), [0], [I]))
     HI = mps.product_mpo(I, N)
     return mps.generate_mpo(HI, terms)
 
@@ -100,10 +100,56 @@ def measure_energy_per_site(psi, a, e0, m, t, L0, v, Q, ops):
         engs[n] += (1 / (4 * a)) * (eSmSp.get((n-1, n), 0) + eSmSp.get((n, n+1), 0)).real
         engs[n] += m * ed[n].real
     for n in range(psi.N - 1):
-        cst = L0 + dQn(n, t, x0, v, Q)
+        cst = L0 + dQn(n, t, x0, a, v, Q)
         tmp = cst * cst
         tmp += 2 * cst * sum(((-1) ** k) * ed[k] for k in range(n + 1))
         tmp += sum(ed[k] for k in range(n + 1))
         tmp += 2 * sum(((-1) ** (k + l)) * edd[k, l] for k in range(n + 1) for l in range(k + 1, n + 1))
         engs[n] += e0 * tmp.real
     return engs
+
+def measure_energy_per_bond(psi, a, e0, m, t, L0, v, Q, ops):
+    Sp, Sm = ops.sp(), ops.sm()
+    d = {0: Sp @ Sm, 1: Sm @ Sp}
+    dd = {n: d[n % 2] for n in range(psi.N)}
+    N = psi.N
+    x0 = set_x0(N)
+    eSpSm = mps.measure_2site(psi, Sp, Sm, psi, bonds='r1')
+    eSmSp = mps.measure_2site(psi, Sm, Sp, psi, bonds='r1')
+    ed = mps.measure_1site(psi, dd, psi)
+    edd = mps.measure_2site(psi, dd, dd, psi, bonds='<')
+    #
+    engs = np.zeros(N - 1, dtype=np.float64)
+    for n in range(N - 1):
+        engs[n] += (1 / (2 * a)) * (eSpSm[n, n+1] + eSmSp[n, n+1]).real
+
+    engs[0] += m * ed[0].real
+    engs[N-2] += m * ed[N-1].real
+
+    for n in range(1, N-1):
+        engs[n] += (m / 2) * ed[n].real
+        engs[n-1] += (m / 2) * ed[n].real
+
+    for n in range(N - 1):
+        cst = L0 + dQn(n, t, x0, a, v, Q)
+        tmp = cst * cst
+        tmp += 2 * cst * sum(((-1) ** k) * ed[k] for k in range(n + 1))
+        tmp += sum(ed[k] for k in range(n + 1))
+        tmp += 2 * sum(((-1) ** (k + l)) * edd[k, l] for k in range(n + 1) for l in range(k + 1, n + 1))
+        if n == 0:
+            engs[n] += e0 * tmp.real
+        else:
+            engs[n-1] += (e0 / 2) * tmp.real
+            engs[n] += (e0 / 2) * tmp.real
+    return engs
+
+def axial_vector_source(N, P, a, ops=None):
+    I, Sp, Sm = ops.I(), ops.sp(), ops.sm()
+    HI = mps.product_mpo(I, N)
+    terms =  [mps.Hterm( np.exp(1j * P * n / a), (n, n + 1), (Sp, Sm)) for n in range(N - 1)]
+    terms += [mps.Hterm(-np.exp(1j * P * n / a), (n + 1, n), (Sp, Sm)) for n in range(N - 1)]
+    pp = mps.generate_mpo(HI, terms)
+    terms =  [mps.Hterm(-np.exp(-1j * P * n / a), (n, n + 1), (Sp, Sm)) for n in range(N - 1)]
+    terms += [mps.Hterm( np.exp(-1j * P * n / a), (n + 1, n), (Sp, Sm)) for n in range(N - 1)]
+    ppdag = mps.generate_mpo(HI, terms)
+    return (1 / (a * a)) * (pp @ ppdag)
---
+++ run_evolution.py
@@ -0,0 +1,140 @@
+import numpy as np
+import csv
+import os.path
+from pathlib import Path
+import time
+import ray
+import yastn
+import yastn.tn.mps as mps
+from operators import HXXZ, sumLn2, Ln, momentum_total, momentum_n, measure_energy_per_site
+
+def folder_gs(g, m, L0, a, N):
+    path = Path(f"./results/{g=:0.4f}/{m=:0.4f}/{L0=:d}/{N=}/{a=:0.4f}/gs/")
+    path.mkdir(parents=True, exist_ok=True)
+    return path
+
+def folder_evol(g, m, L0, a, N, v, Q):
+    path = Path(f"./results/{g=:0.4f}/{m=:0.4f}/{L0=:d}/{N=}/{a=:0.4f}/{v=:0.4f}/{Q=:0.4f}/")
+    path.mkdir(parents=True, exist_ok=True)
+    return path
+
+@ray.remote(num_cpus=2)
+def run_gs(g, m, L0, a, N, D0, energy_tol=1e-12, Schmidt_tol=1e-10):
+    """ initial state at t=0 """
+    #
+    folder = folder_gs(g, m, L0, a, N)
+    fname = folder / f"state_D={D0}.npy"
+    finfo = folder / "info.csv"
+    #
+    e0 = a * g * g / 2
+    t = 0
+    #
+    ops = yastn.operators.Spin12(sym='U1')
+    H0 = HXXZ(N, a, m, ops=ops)
+    H1 = e0 * sumLn2(N, t, L0, a, v=1, Q=1, ops=ops)
+    #
+    files = list(folder.glob("*.npy"))
+    Ds = [int(f.stem.split("=")[1]) for f in files]
+    if any(D <= D0 for D in Ds):
+        D = min(D for D in Ds if D <= D0)
+        print(f"Loading initial state with {D=}")
+        old_data = np.load(folder / f"state_D={D}.npy", allow_pickle=True).item()
+        psi_gs = mps.load_from_dict(ops.config, old_data["psi"])
+    else:
+        print(f"Random initial state.")
+        psi_gs = mps.random_mps(H0, D_total=D0, n=0)
+    # 2 sweeps of 2-site dmrg
+    info = mps.dmrg_(psi_gs, [H0, H1], max_sweeps=200,
+                     method='2site', opts_svd={"D_total": D0},
+                     energy_tol=energy_tol, Schmidt_tol=Schmidt_tol)
+    #
+    data = {}
+    data["psi"] = psi_gs.save_to_dict()
+    data["bd"] = psi_gs.get_bond_dimensions()
+    data["entropy"] = psi_gs.get_entropy()
+    sch = psi_gs.get_Schmidt_values()
+    data["schmidt"] = [x.data for x in sch]
+    np.save(fname, data, allow_pickle=True)
+
+    fieldnames = ["D", "energy", "sweeps", "denergy", "dSchmidt", "min_Schmidt"]
+    out = {"D" : max(data["bd"]),
+           "energy": info.energy,
+           "sweeps": info.sweeps,
+           "denergy": info.denergy,
+           "dSchmidt": info.max_dSchmidt,
+           "min_Schmidt": min(data["schmidt"][N // 2])}
+    file_exists = os.path.isfile(finfo)
+    with open(finfo, 'a', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=";")
+        if not file_exists:
+            writer.writeheader()
+        writer.writerow(out)
+
+@ray.remote(num_cpus=2)
+def run_evol(g, m, L0, a, N, D0, v, Q, dt, snapthots):
+    ops = yastn.operators.Spin12(sym='U1')
+    #
+    try:
+        fname = folder_gs(g, m, L0, a, N) / f"state_D={D0}.npy"
+        data = np.load(fname, allow_pickle=True).item()
+        psi = mps.load_from_dict(ops.config, data["psi"])
+    except FileNotFoundError:
+        return None
+    #
+    e0 = a * g * g / 2
+    t = 0
+    engs_gs = measure_energy_per_site(psi, a, e0, m, t, L0, v, Q, ops)
+
+    folder = folder_evol(g, m, L0, a, N, v, Q)
+
+    ops = yastn.operators.Spin12(sym='U1')
+    H0 = HXXZ(N, a, m, ops=ops)
+    Ht = lambda t: [H0, e0 * sumLn2(N, t, L0, a, v, Q, ops=ops)]
+
+
+    times = np.linspace(0, N * a / (2 * v), snapthots + 1)
+
+    for step in mps.tdvp_(psi, Ht, times, dt=dt,
+                          method='2site', opts_svd={"D_total": D0}):
+        engs = measure_energy_per_site(psi, a, e0, m, step.tf, L0, v, Q, ops)
+        ents = psi.get_entropy()
+
+        deng = engs - engs_gs
+        with open(folder / "engs.txt", "a") as f:
+            f.write(f"{step.tf:0.2f}")
+            for ee in deng:
+                f.write(f";{ee:0.6f}")
+            f.write("\n")
+
+        with open(folder / "ents.txt", "a") as f:
+            f.write(f"{step.tf:0.2f}")
+            for ee in ents:
+                f.write(f";{ee:0.6f}")
+            f.write("\n")
+
+
+if __name__ == "__main__":
+    #
+    g = 1 / 5
+    # m = g / 8
+    # a = 1
+    L0 = 0
+    D0 = 64
+
+    # N = 640
+    # # for m in [0*g, 0.25*g, 0.5*g]:
+    #     for a in [1, 0.25]:
+
+    # refs = []
+    # for m in [0*g, 0.125 * g, 0.25 * g, 0.5 * g]:
+    #     for N, a in [(100, 1.0), (200, 0.5), (400, 0.25)]:
+    #         job = run_gs.remote(g, m, L0, a, N, D0, energy_tol=1e-12, Schmidt_tol=1e-8)
+    #         refs.append(job)
+    # ray.get(refs)
+
+    refs = []
+    for m in [0*g, 0.125 * g, 0.25 * g, 0.5 * g]:
+        for N, a in [(100, 1.0), (200, 0.5), (400, 0.25)]:
+            job = run_evol.remote(g, m, L0, a, N, D0, 1, 1, 1/8, N // 2)
+            refs.append(job)
+    ray.get(refs)
